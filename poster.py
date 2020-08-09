56ddf94806cc34347eb0b4a37496c555bad2c796
davearias <davearias@gmail.com>
2020-08-09 16:46:11 -0400

    Made changes to html page, better suited for actual use. Hard coded script directory path to '/home/pi/uberPoster' so that script can be run at reboot via crontab. Made changes to use new 'cwd' path reference. Added 'quit' command to give a way to quit out of uberPoster after disabling keyboard input (so that script can be run at reboot via crontab) Added "enKeyboard" option to enable/disable keyboard input

--- poster.py
+++ poster.py
@@ -3,25 +3,31 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import pi3d
+#import os
+import sys
 
 posterTex = "default.jpg"
 fps = 30
 logLevel = 'critical' #debug, info are used, warning, error & critical will disable logging
 logFile = 'poster.log'
+enKeyboard = 0
 
-logger = pi3d.Log(level=logLevel,file='poster.log')
+#cwd = os.getcwd()
+cwd = "/home/pi/uberPoster"
+logger = pi3d.Log(level=logLevel,file=cwd+"/"+logFile)
 DISPLAY = pi3d.Display.create(background=(0.0, 0.0, 0.0, 1.0),frames_per_second=fps)
 shader = pi3d.Shader("uv_flat")
 CAMERA = pi3d.Camera(is_3d=False)
 CAMERA.rotateZ(90)
-tex1 = pi3d.Texture(posterTex)
+tex1 = pi3d.Texture(cwd+"/"+posterTex)
 z1 = 5.0 # z value of sprite1
 a1 = 0.0 # alpha value of sprite1
 alpha_delta = 0.05
 sprite1 = pi3d.ImageSprite(tex1,shader=shader,w=DISPLAY.height, h=DISPLAY.width, z=z1)
 sprite1.set_alpha(a1)
 
-keys = pi3d.Keyboard()
+if enKeyboard == 1:
+  keys = pi3d.Keyboard()
 
 command = 1
 
@@ -42,13 +48,27 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<p><b>Current Poster: </b>"+posterTex+"</p>", "utf-8"))
         self.wfile.write(bytes("<p><b>Current Command: </b>"+str(command)+"</p>", "utf-8"))
         self.wfile.write(bytes("<p><b>Current Rate: </b>"+str(round((1/alpha_delta)/fps,1))+"s</p>", "utf-8"))
-        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=1"">Fade Up Current Poster</a></p>", "utf-8"))        
+        self.wfile.write(bytes("<br><p><b>Command Reference:</b></p>", "utf-8"))
+        self.wfile.write(bytes("<p><b>cmd=0</b> will fade out the current poster</p>", "utf-8"))
+        self.wfile.write(bytes("<p><b>cmd=1</b> will fade up the current poster</p>", "utf-8"))
+        self.wfile.write(bytes("<p><b>rate=#</b> will set the fade rate in seconds to the value of #</p>", "utf-8"))
+        self.wfile.write(bytes("<p><b>poster=filename.ext</b> will change the poster to the new filename (must be in uberPoster directory)</p>", "utf-8"))
+        self.wfile.write(bytes("<p><b>quit=1</b> will quit uberPoster</p>", "utf-8"))
+        self.wfile.write(bytes("<p>URL command string must begin with <b>?</b> and multiple commands must be separated with <b>&</b></p>", "utf-8"))
+
+        self.wfile.write(bytes("<br><p><b>Sample Command:</b></p>", "utf-8"))
+        self.wfile.write(bytes("<p>http://"+ip+":8000/?cmd=0&rate=5 will fade the poster out with a 5 second fade time", "utf-8"))
+        self.wfile.write(bytes("<p>http://"+ip+":8000/?poster=2001ASO.jpg will switch the poster to an image named \"2001ASO.jpg\"</p>", "utf-8"))
+       
+        self.wfile.write(bytes("<br><p><a href=http://"+ip+":8000/?cmd=1"">Fade Up Current Poster</a></p>", "utf-8"))        
         self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=0"">Fade Down Current Poster</a></p>", "utf-8"))        
-        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=0&poster=poster.jpg"">Change to Poster 1 in Black</a></p>", "utf-8"))        
-        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=0&poster=poster2.jpg"">Change to Poster 2 in Black</a></p>", "utf-8"))        
-        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=1&poster=poster.jpg"">Change to Poster 1, Alpha=1</a></p>", "utf-8"))        
-        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=1&poster=poster2.jpg"">Change to Poster 2, Alpha=1</a></p>", "utf-8"))        
+#        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=0&poster=poster.jpg"">Change to Poster 1 in Black</a></p>", "utf-8"))        
+#        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=0&poster=poster2.jpg"">Change to Poster 2 in Black</a></p>", "utf-8"))        
+#        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=1&poster=poster.jpg"">Change to Poster 1, Alpha=1</a></p>", "utf-8"))        
+#        self.wfile.write(bytes("<p><a href=http://"+ip+":8000/?cmd=1&poster=poster2.jpg"">Change to Poster 2, Alpha=1</a></p>", "utf-8"))        
         self.wfile.write(bytes("<br><p><a href=http://"+ip+":8000/""><b>REFRESH</b></a></p>", "utf-8"))        
+        self.wfile.write(bytes("<br><p><a href=http://"+ip+":8000/?quit=1""><b>QUIT uberPoster</b></a></p>", "utf-8"))        
+
 
     def do_GET(self):
         self.send_response(200)
@@ -71,7 +91,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
            if "poster" in query:
               if query['poster'][0] != posterTex:
                 posterTex = query['poster'][0]
-                sprite1 = pi3d.ImageSprite("./"+posterTex,shader=shader,w=DISPLAY.height, h=DISPLAY.width, z=z1)
+                sprite1 = pi3d.ImageSprite(cwd+"/"+posterTex,shader=shader,w=DISPLAY.height, h=DISPLAY.width, z=z1)
                 if a1 <= 0:
                   sprite1.set_alpha(0.0)
                   a1 = 0.0                            
@@ -88,7 +108,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
            if "rate" in query:
               alpha_delta = 1 / (float(query['rate'][0]) * fps)
               logger.debug("Rate now: %f", alpha_delta)
-
+           if "quit" in query:
+              logger.debug("Quit received")
+              if query["quit"] == ['1']:
+                DISPLAY.destroy()
+                sys.exit(0)
 
     def log_message(self, format, *args):
         return
@@ -108,22 +132,22 @@ thread.start()
 
 while DISPLAY.loop_running():
   sprite1.draw()
-
-  k = keys.read()
-  if k > -1:
-    if k == 27:
+  if enKeyboard == 1:
+    k = keys.read()
+    if k > -1:
+      if k == 27:
+        logger.debug('Key received %s',chr(k))
+        break
+      elif k == ord('a'): # less solid sprite1
+        a1 -= 0.05
+      elif k == ord('d'): # more solid sprite1
+        a1 += 0.05
+        sprite1.set_alpha(a1)
+      elif k == ord('u'): # more solid sprite1
+        command=1
+      elif k == ord('f'): # more solid sprite1
+        command=2
       logger.debug('Key received %s',chr(k))
-      break
-    elif k == ord('a'): # less solid sprite1
-      a1 -= 0.05
-    elif k == ord('d'): # more solid sprite1
-      a1 += 0.05
-      sprite1.set_alpha(a1)
-    elif k == ord('u'): # more solid sprite1
-      command=1
-    elif k == ord('f'): # more solid sprite1
-      command=2
-    logger.debug('Key received %s',chr(k))
 
 #Fade Up
   if command==1:
@@ -141,7 +165,7 @@ while DISPLAY.loop_running():
       logger.debug('Alpha=%f',a1)
 
     
-keys.close()
+if enKeyboard==1:keys.close()
 DISPLAY.destroy()
 
 
